8eb3670bfd35db7e39449082968fb4a6a893d438
Calistus  Chukwuebuka Ndubuisi <135891169+romeofx@users.noreply.github.com>
2025-06-30 09:52:07 +0000

    app update

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def participants():
 
 if __name__ == "__main__":
     # Disable debug in production!
-    app.run(host="0.0.0.0", port=10000, debug=True
+    app.run(host="0.0.0.0", port=10000, debug=True)
